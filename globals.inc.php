89abd433346147585af6a0d1e5444f14b5b13568
Florian <pouchelet.florian@orange.fr>
2018-04-14 09:25:14 +0200

    feat(Inscription): ajoute une globale de succès pour les inscriptions

--- globals.inc.php
+++ globals.inc.php
@@ -3,6 +3,7 @@
 define("DB_TEAMS", "teams");
 define("DB_MATCHES", "matches");
 
-define("MISSING_FIELDS", 1);
-define ("TEAM_EXISTS", 2);
+define("REGISTRATION_OK", 1);
 define("MAX_PLAYER_TEAM", 2);
+define("MISSING_FIELDS", 3);
+define ("TEAM_EXISTS", 4);
